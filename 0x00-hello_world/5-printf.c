7d02e05edb183250e95dd28b2a66a605f719cf68
Eric Karanja <erickaranja54@gmail.com>
2023-03-16 13:01:29 -0700

    printf("size of a long int: %lu byte(s)\n",(unsigned long)sizeof(z));

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 /*
  * main - Entry point
- *
+ * returns with proper grammar, but the outcome is a piece of art
  * Return: always 0 (success)
  */
 int main(void)
---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+/*
+ *main-entry
+ * program that outputs the size of various computer types
+ *
+ * return 0 (success)
+ */
+int main(void)
+{
+	char x;
+	int y;
+	long int z;
+	long long int d;
+	float f;
+
+printf("size of a char: %lu byte(s)\n",(unsigned long)sizeof(x));
+printf("size of an int: %lu byte(s)\n",(unsigned long)sizeof(y));
+printf("size of a long int: %lu byte(s)\n",(unsigned long)sizeof(z));
+printf("size of a long long int: %lu byte(s)\n",(unsigned long)sizeof(d));
+printf("size of a float: %lu byte(s)\n",(unsigned long)sizeof(f));
+return (0);
+}

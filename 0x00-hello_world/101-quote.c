7b5add4f14d45de0595463e4bf293264acd7a49a
Eric Karanja <erickaranja54@gmail.com>
2023-03-20 00:47:58 -0700

    6-size.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,9 +1,10 @@
 #include<stdio.h>
-/*
- * main prints 'and that piece of art is useful" - Dora Korpar, 2015-10-19'
- *
- * return 0 (success)
+
+/**
+ * main - prints 'and that piece of art is useful" - Dora Korpar, 2015-10-19'
+ * Return: 0 (success)
  */
+
 int main(void)
 {
 write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);

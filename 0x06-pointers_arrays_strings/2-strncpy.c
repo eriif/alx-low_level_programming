827052dad8bd5e35ebdfa1a11a6b6ee9ba1b0c55
Eric Karanja <erickaranja54@gmail.com>
2023-03-30 22:55:02 +0300

    2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -9,21 +9,18 @@
 
 char *_strncat(char *dest, char *src, int n)
 {
-	int i;
 	int j;
 
-	i = 0;
-	while (dest[i] != '\0')
-	{
-		i++;
-	}
 	j = 0;
-	while (j < n && src[j] != '\0')
+	while (j < n && src[j] ! = '\0')
 	{
-		dest[i] = src[j];
-		i++;
+		dest[j] = src[j];
 		j++;
 	}
-	dest[i] = '\0';
+	while (j < n)
+	{
+		dest[j] = '\0';
+		j++;
+	}
 	return (dest);
 }

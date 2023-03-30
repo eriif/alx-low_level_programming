a5be07511612ff9c169dda738012c96aa8b24f04
Eric Karanja <erickaranja54@gmail.com>
2023-03-30 23:00:05 +0300

    Alx SE

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,18 +1,18 @@
 #include "main.h"
 /**
- * _strncat - concentrates two strings
+ * _strncpy - concentrates two strings
  * @dest: input value
  * @n: input value
  * @src: input value
  * Return: dest
  */
 
-char *_strncat(char *dest, char *src, int n)
+char *_strncpy(char *dest, char *src, int n)
 {
 	int j;
 
 	j = 0;
-	while (j < n && src[j] ! = '\0')
+	while (j < n && src[j] != '\0')
 	{
 		dest[j] = src[j];
 		j++;

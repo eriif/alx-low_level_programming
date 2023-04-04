8578a9fb9a3c6bd04fcd2201da11672e0ae9c48b
Eric Karanja <erickaranja54@gmail.com>
2023-04-04 04:13:47 +0300

    0-memset.c

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+* _memcpy - Entry point
+* @dest: input
+* @src: input value
+* @n: input value
+* Return: Always 0 (success)
+*/
+char *_memcpy(char *dest, char *src, unsigned int n);
+{
+	unsigned int i;
+	for (i = 0; i < n; i++)
+	dest[i] = src[i];
+	return (dest);
+}

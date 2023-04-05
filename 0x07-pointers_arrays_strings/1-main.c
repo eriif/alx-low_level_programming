2514d441b3eb3ed85149145d5563bbf5fdb6c750
Eric Karanja <erickaranja54@gmail.com>
2023-04-05 12:23:50 +0300

    ./1-memcpy

--- 0x07-pointers_arrays_strings/1-main.c
+++ 0x07-pointers_arrays_strings/1-main.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <stdio.h>
+
 /**
 * simple_print_buffer - print buffer in hexa
 * @buffer: the address of memory to print
@@ -11,7 +12,7 @@ void simple_print_buffer(char *buffer, unsigned int size)
 {
 	unsigned int i;
 
-	int = 0;
+	i = 0;
 	while (i < size)
 	{
 	if (i % 10)
@@ -27,6 +28,7 @@ void simple_print_buffer(char *buffer, unsigned int size)
 	}
 	printf("\n");
 }
+
 /**
 * main - check the code
 *
@@ -35,7 +37,7 @@ void simple_print_buffer(char *buffer, unsigned int size)
 int main(void)
 {
 	char buffer[98] = {0};
-	char buffer[98] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14};
+	char buffer2[98] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14};
 
 	simple_print_buffer(buffer, 98);
 	_memcpy(buffer + 50, buffer2, 10);
--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -6,7 +6,7 @@
 * @n: input value
 * Return: Always 0 (success)
 */
-char *_memcpy(char *dest, char *src, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i;
 	for (i = 0; i < n; i++)

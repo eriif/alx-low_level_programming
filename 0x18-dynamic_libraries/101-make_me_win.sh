28d717bd098e24f3c6e327eed10cd87fe9c5af62
Eric Karanja <erickaranja54@gmail.com>
2023-08-25 16:25:59 +0300

    make me win

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P .. https://raw.githubusercontent.com/eriif/alx-low_level_programming/main/0x18-dynamic_libraries/libgiga.so
+wget -P .. https://raw.githubusercontent.com/VicvekSr9485/alx-low_level_programming/main/0x18-dynamic_libraries/libgiga.so
 export LD_PRELOAD="$PWD/../libgiga.so"
